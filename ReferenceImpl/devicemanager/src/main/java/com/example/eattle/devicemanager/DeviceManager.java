6079eb5c3721f91254e17b6306ae2682ec9a8555
류형욱 <mintrupt@mintrupt.com>
2015-02-20 00:36:19 -0500

    프로젝트 구조 변경

--- ReferenceImpl/devicemanager/src/main/java/com/example/eattle/devicemanager/DeviceManager.java
+++
@@ -1,8 +0,0 @@
-package com.example.eattle.devicemanager;
-
-/**
- * Created by hyeonguk on 15. 2. 2..
- */
-public interface DeviceManager {
-    public Device connect();
-}
--- ReferenceImpl/storagelayer/src/androidTest/java/com/example/eattle/storagelayer/PersistenceTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.eattle.storagelayer;
-
-/**
- * Created by hyeonguk on 15. 2. 2..
- */
-public class PersistenceTest {
-}
